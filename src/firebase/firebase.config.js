5cdc8ddc2a08e8b2e66ef5c117b2709c2e29ad17
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-11-05 14:36:09 +0600

    install needed inital packages

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHki1vm5buXRWkh1Viw0IjguXLPMXUUjE",
+  authDomain: "foodie-pal-7e474.firebaseapp.com",
+  projectId: "foodie-pal-7e474",
+  storageBucket: "foodie-pal-7e474.appspot.com",
+  messagingSenderId: "319130736129",
+  appId: "1:319130736129:web:aad0641a911b606a8dd200",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
