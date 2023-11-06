b73c2efd9f4a0dd087a3abe47944ec517e9854fd
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-11-07 01:20:03 +0600

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,22 @@
-
+import Lottie from "lottie-react";
+import gif from "../assets/Animation - 1699174502851.json";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div className="h-[100vh] grid place-content-center">
-             <h1 className="text-3xl text-red-500 font-bold">PAGE NOT FOUND</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center gap-5">
+      <div className="flex justify-center flex-col items-center">
+        <h1 className="text-3xl text-[#d03300] uppercase font-bold">This PAGE NOT FOUND!</h1>
+        <Link to="/">
+          {" "}
+          <h3 className="text-2xl underline cursor-pointer text-green-500 font-semibold">
+            Back to home page
+          </h3>
+        </Link>
+      </div>
+      <Lottie className="w-1/3 mt-12" animationData={gif}></Lottie>
+    </div>
+  );
 };
 
 export default ErrorPage;
